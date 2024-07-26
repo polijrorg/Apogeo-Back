c8b2ce2445a7847b9d5367b21dcac8eb6652dc77
Tássyla Lissa Lima <tassyla.tll@gmail.com>
2024-07-26 20:46:24 -0300

    feat: forgot password

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,38 @@
+import { inject, injectable } from 'tsyringe';
+
+import { Users } from '@prisma/client';
+
+import AppError from '@shared/errors/AppError';
+
+import IHashProvider from '@shared/container/providers/HashProvider/models/IHashProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequest {
+  id: string;
+  password: string;
+}
+
+@injectable()
+export default class UpdateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) { }
+
+public async execute({
+  id,
+  password,
+}: IRequest): Promise<Users> {
+    const userAlreadyExists = await this.usersRepository.findById(id);
+
+    if (!userAlreadyExists) throw new AppError('User with this id does not exist');
+
+    const hashedPassword = await this.hashProvider.generateHash(password);
+    const updatedUser = await this.usersRepository.resetPassword(id, hashedPassword);
+
+    return updatedUser;
+  }
+}
