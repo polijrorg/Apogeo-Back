92da0dce12a32f9078846f449c7df12e7481e1e8
Tássyla Lissa Lima <tassyla.tll@gmail.com>
2024-09-18 17:56:57 -0300

    feat: make appointments

---
+++ src/modules/users/infra/http/controller/AppointmentsController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import CreateAppointmentService from '@modules/users/services/CreateAppointmentService';
+
+export default class AppointmentsController {
+
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { id } = req.token;
+    const { date } = req.body;
+
+    const readUser = container.resolve(CreateAppointmentService);
+
+    const user = await readUser.execute({
+      id,
+      date
+    });
+
+    return res.status(201).json({
+      ...user,
+      password: undefined,
+      pin: undefined,
+      pinExpires: undefined,
+      pedigree: undefined,
+    });
+  }
+}
